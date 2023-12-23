b53e1c5ad4ecf519197a5583f809318e55869fda
Catur Hidayat <caturhidayat00@gmail.com>
2023-12-23 19:05:40 +0700

    Fix: Error: Parsing error: Unexpected token. Did you mean `{'>'}` or `&gt;`? (#4)
    * feat: reset input field (#3)
    
    * feat: scan QR and validate, and then render in FE (#1)
    
    * feat[WIP]: add API to handle POST Validate URL Faktur
    
    * feat: add API to handle POST Validate URL Faktur adn return back to FE
    
    * refactor: change import of xlsx
    
    * feat: reset input after submit and loading state is submit occured
    
    * fix: Error: Parsing error: Unexpected token

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,7 +79,25 @@ export default function Home() {
                         )}
                     />
                     <button type='submit' className='btn btn-accent'>
-                        Validate
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='none'
+                            viewBox='0 0 24 24'
+                            strokeWidth='1.5'
+                            stroke='currentColor'
+                            className='w-6 h-6'
+                        >
+                            <path
+                                strokeLinecap='round'
+                                strokeLinejoin='round'
+                                d='m4.5 12.75 6 6 9-13.5'
+                            />
+                        </svg>
+                        {formik.isSubmitting ? (
+                            <span className='loading loading-spinner loading-md'></span>
+                        ) : (
+                            "Validate"
+                        )}
                     </button>
                 </div>
             </form>
@@ -88,7 +106,6 @@ export default function Home() {
                     onClick={handleExport}
                     className='btn btn-outline btn-accent'
                 >
-
                     <svg
                         xmlns='http://www.w3.org/2000/svg'
                         fill='none'
@@ -103,7 +120,6 @@ export default function Home() {
                             d='M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m.75 12 3 3m0 0 3-3m-3 3v-6m-1.5-9H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z'
                         />
                     </svg>
-
                     Export
                 </button>
             </div>
